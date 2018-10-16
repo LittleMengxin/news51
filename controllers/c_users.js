ed13a3a60f628c951fa4ff27ac64806d5521b47d
liuzhenxi <liuzhenxidaren@163.com>
2018-10-16 08:49:24 +0800

    第二次修改

--- controllers/c_users.js
+++ controllers/c_users.js
@@ -12,5 +12,19 @@ exports.shouSignin = (req, res)=>{
 //登陆请求
 exports.handleSignin = (req, res)=>{
     const body = req.body
-    alert('测试成功')
+
+
+    const sqlstr = "SELECT *FROM 'users' WHERE email = ?"
+    connection.query(sqlstr, body.email, (err, data)=>{
+        if(err){
+            return res.send(err)
+        }
+        if(!data[0]){
+            return res.send('该邮箱不存在，快注册一个')
+        }
+        if(boyd.password != data[0].password){
+            return res.send('密码不正确')
+        }
+        res.redirect('./')
+    })
 }
